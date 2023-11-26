85637bfc7031180e4ff0c37cd3b9d245daae7d29
Viktoriia <pecourses>
2023-11-26 15:54:34 +0200

    Add server with index.html

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const { readFile } = require('fs/promises');
 
 // 1 Create server
 const server = http.createServer((request, response) => {
-  // console.log('Hello from server');
   console.log('request.url :>> ', request.url);
   console.log('request.method :>> ', request.method);
+  // / => homepage
+  // /about => aboutpage
 
-  response.statusCode = 200;
-  response.end('result');
+  readFile('./pages/index.html', { encoding: 'utf-8' })
+    .then(fileText => {
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end(fileText);
+    })
+    .catch(err => {
+      response.statusCode = 500;
+      response.end('error');
+    });
 });
 
 // 2 Run server
